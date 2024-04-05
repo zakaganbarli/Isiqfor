f9872ce08ccca8f4d7b289b6b9643e40c04db977
zeka qenberli <ganbarlizaka@gmail.com>
2024-04-05 09:42:58 +0400

    isiqfor

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Rengi daxil edin");
+        String isiq = scanner.nextLine();
+
+        switch (isiq) {
+            case "sari":
+                System.out.println("Hazirlasin");
+                break;
+
+            case "qirmizi":
+                System.out.println("Dayanin");
+                break;
+
+            case "yasil":
+                System.out.println("Hereket edin");
+                break;
+
+            default:
+                System.out.println("Sfetefor islemir");
+        }
+    }
+}
